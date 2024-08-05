9acda55e874016329f51a526c823ec09d9318302
unify-z <3336016933@qq.com>
2024-08-05 13:16:08 +0800

    github action update

---
+++ delopy.cmd
@@ -0,0 +1,8 @@
+
+cd D:\UserData\Documents\hexo-blog&&hexo clean&&hexo g&&git add .&&git commit -m "github action update" &&git push -f origin master&&pause
+
+
+
+
+
+
--- src-old/delopy.cmd
+++
@@ -1,8 +0,0 @@
-
-cd D:\UserData\Documents\hexo&&hexo clean&&hexo g&&git add .&&git commit -m "github action update" &&git push -f origin master&&pause
-
-
-
-
-
-
